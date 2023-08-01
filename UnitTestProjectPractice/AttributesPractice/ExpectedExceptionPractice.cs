78df5d5bdf223fa033f4fc54a229eae402b8643d
11Sura <shamaru12196@gmail.com>
2023-08-01 17:40:35 +0530

    new commit: ExpectedExceptionsPractice class added

---
+++ UnitTestProjectPractice/AttributesPractice/ExpectedExceptionPractice.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProjectPractice.AttributesPractice
+{
+    [TestClass]
+    public class ExpectedExceptionPractice
+    {
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]  //the type of exception must be specified here
+        public void ExpectedExceptionsMain()
+        {
+            int[] a = { 2, -4, 23, 5 };
+            for(int i=0; i<=a.Length; i++)    //index is out of bounds, so exception expected here
+            {
+                Console.WriteLine(a[i]);
+            }
+
+        }
+
+    }
+}
---
+++ UnitTestProjectPractice/AttributesPractice/PriorityPractice.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProjectPractice.AttributesPractice
+{
+    [TestClass]
+   
+    [TestCategory("priority")]
+    public class PriorityPractice
+    {
+       
+        [TestMethod]
+        [Priority(3)]
+        public void PriorityMain1()
+        {
+            Console.WriteLine("first method");
+        }
+
+        [TestMethod]
+        [Priority(0)]
+        public void PriorityMain2()
+        {
+            Console.WriteLine("second method");
+        }
+
+        [TestMethod]
+        [Priority(2)]
+        public void PriorityMain3()
+        {
+            Console.WriteLine("third method");
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        public void PriorityMain4()
+        {
+            Console.WriteLine("fourthmethod");
+        }
+    }
+}
